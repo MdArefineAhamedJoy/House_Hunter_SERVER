acb5070cd2b68baf6d354b4091bde3cafcf3e7c5
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-07-18 01:54:46 +0600

    add monogdb and fake pagination

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
-const express = require('express')
-const app = express()
-const cors = require("cors")
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.p45io4t.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const storeHouse = client.db("HouseHunter").collection("House");
+
+    app.get("/api/houses", async (req, res) => {
+      const page = parseInt(req.query.page) || 1;
+      const perPage = 10;
+      const totalHouses = await storeHouse.countDocuments();
+      const skip = (page - 1) * perPage;
+      const houses = await storeHouse
+        .find()
+        .skip(skip)
+        .limit(perPage)
+        .toArray();
+      res.json({
+        houses,
+        currentPage: page,
+        totalPages: Math.ceil(totalHouses / perPage),
+      });
+    });
 
 
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World! House Hunting Start ')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! House Hunting Start ");
+});
 
 app.listen(port, () => {
-  console.log(` listening on port ${port}`)
-})
-
-
+  console.log(` listening on port ${port}`);
+});
--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,23 +1,32 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userDataSchema = mongoose.Schema({
-    name:{
-        type:String,
-        require : true
+const userDataSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
     },
-    username : {
-        type : String ,
-        require : true
+    username: {
+      type: String,
+      require: true,
     },
-    password : {
-        type : String ,
-        require : true
+    email : {
+      type: String,
+      require: true,
+      unique : true
     },
-    status : {
-        type : String ,
-        enum : ['active' , "inactive"]
-    }
-})
+    password: {
+      type: String,
+      require: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+    },
+  },
+  {collection : 'user-data'}
+);
 
+const model = mongoose.model("UserData" , userDataSchema)
 
-module.exports = userDataSchema ;
+module.exports = userDataSchema;
